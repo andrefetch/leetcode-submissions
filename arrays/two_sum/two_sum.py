3e4d29a7a49a5ffe853b13a27389a3f1a76f85df
Andre Nunes da Silva <andresilvapersonal2007@gmail.com>
2026-08-05 19:05:19 -0400

    Two Sum

---
+++ arrays/two_sum/two_sum.py
@@ -0,0 +1,16 @@
+from typing import List
+
+'''
+Will improve my o(n^2) solution
+'''
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    
+        for i in range(len(nums) - 1):
+
+            for j in range(i + 1, len(nums)):
+
+                if nums[i] + nums[j] == target:
+
+                    return [i, j]
